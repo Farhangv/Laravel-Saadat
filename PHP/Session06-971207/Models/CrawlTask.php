69cd30487d76b9047e0ce6953a4e7c79768d066f
Farhang Vafa <farhangv@ymail.com>
2019-02-26 22:49:45 +0330

    971207

---
+++ PHP/Session06-971207/Models/CrawlTask.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Crawler\Models;
+
+class CrawlTask
+{
+    public $name;
+    public $urls;
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of urls
+     */ 
+    public function getUrls()
+    {
+        return $this->urls;
+    }
+
+    /**
+     * Set the value of urls
+     *
+     * @return  self
+     */ 
+    public function setUrls($urls)
+    {
+        $this->urls = $urls;
+
+        return $this;
+    }
+}
+
---
+++ PHP/Session06-971207/Services/FormService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Crawler\Services;
+
+class FormService
+{
+    public static function test():void
+    {
+        echo 'test';
+    }
+}
